49830d00d5cf4c35a308321b52b6c6a6c2af4fc4
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2023-08-23 12:38:36 -0300

    creating form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Container, Grid, Typography } from '@mui/material';
+import Formulario from './components/Formulario';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Container>
+      <header>
+        <Typography align='center' marginY={5} component="h1" variant='h3'>
+          Buscador de noticias
+        </Typography>
+      </header>
+      <Grid container direction="row" justifyContent="center" alignItems="center">
+        <Grid item xs={12} md={6}>
+          <Formulario />
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FormControl, InputLabel, Select, MenuItem, Button, Box } from '@mui/material';
+
+const categorias = [ // no se puede agregar mas categorias, esto es lo que soporta la API de material UI
+    { value: 'general', label: 'General' },
+    { value: 'business', label: 'Negocios' },
+    { value: 'entertainment', label: 'Entretenimiento' },
+    { value: 'health', label: 'Salud' },
+    { value: 'science', label: 'Ciencia' },
+    { value: 'sports', label: 'Deportes' },
+    { value: 'technology', label: 'Tecnología' },
+]
+
+const Formulario = () => {
+
+
+
+    return (
+        <form>
+            <FormControl fullWidth>
+                <InputLabel>Categoria</InputLabel>
+                <Select label="categoria">
+                    {categorias.map((categoria) => (
+                        <MenuItem
+                            key={categoria.value} value={categoria.value}>
+                            {categoria.label}
+                        </MenuItem>
+                    ))}
+                </Select>
+                <Box sx={{ marginTop: 2 }}>
+                    <Button fullWidth variant='contained'>Buscar noticias</Button>
+                </Box>
+            </FormControl>
+        </form>
+    )
+}
+
+export default Formulario
